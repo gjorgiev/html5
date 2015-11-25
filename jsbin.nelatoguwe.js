21362bacd615a7c767cdb633f0dc7b7659fb5904
Gjorgi Gjorgiev <joey.jimi@yahoo.com>
2015-11-25 15:24:51 +0100

    Update jsbin.nelatoguwe.js

--- jsbin.nelatoguwe.js
+++ jsbin.nelatoguwe.js
@@ -1,5 +1,15 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+ctx.strokeStyle = '28d1fa';
+ctx.lineWidth = 17;
+ctx.lineCap = 'round';
+ctx.shadowBlur = 15;
+ctx.shadowColor = '28d1fa';
+
+function degToRad(degree){
+  var factor = Math.PI/180;
+  return factor*degree;
+}
 
 function renderTime(){
   
@@ -10,22 +20,27 @@ function renderTime(){
   var minutes = now.getMinutes();
   var seconds = now.getSeconds();
   var milliseconds = now.getMilliseconds();
+  var newSeconds = seconds + (milliseconds/1000);
   
   //Background 
-  
+  ctx.fillStyle = '333333';
+  ctx.fillRect(0,0,500,500);
   
   
   //Hours 
-  
-  
+  ctx.beginPath();
+  ctx.arc(250,250,200,degToRad(270),degToRad((hours*15)-90));
+  ctx.stroke();
   
   //Minutes 
-  
-  
+  ctx.beginPath();
+  ctx.arc(250,250,170,degToRad(270),degToRad((minutes*6)-90));
+  ctx.stroke();
   
   //Seconds
-  
-  
+  ctx.beginPath();
+  ctx.arc(250,250,140,degToRad(270),degToRad((newSeconds*6)-90));
+  ctx.stroke();
   
   //Date 
 
@@ -33,5 +48,4 @@ function renderTime(){
   
   //Time
 }
-
-renderTime();
+setInterval(renderTime,40);
