dd83c085e856e1c7c31d9ce566fcbb4936b77832
Gjorgi Gjorgiev <joey.jimi@yahoo.com>
2015-11-25 14:26:21 +0100

    Update jsbin.nelatoguwe.js

--- jsbin.nelatoguwe.js
+++ jsbin.nelatoguwe.js
@@ -3,6 +3,35 @@ var ctx = canvas.getContext('2d');
 
 function renderTime(){
   
+  var now = new Date();
+  var today = now.toDateString();
+  var time = now.toLocaleTimeString();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var milliseconds = now.getMilliseconds();
+  
+  //Background 
   
   
+  
+  //Hours 
+  
+  
+  
+  //Minutes 
+  
+  
+  
+  //Seconds
+  
+  
+  
+  //Date 
+
+  
+  
+  //Time
 }
+
+renderTime();
